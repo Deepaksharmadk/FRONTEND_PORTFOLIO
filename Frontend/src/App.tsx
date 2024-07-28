12f9f4859bb3f8c8b049b9a9db66733e530727f5
deepaksharmadk <dks847203@gmail.com>
2024-07-28 15:34:26 +0530

    add shadcn ui

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl bg-yellow-500 text-red-700 font-bold underline">
-        Hello world!
-      </h1>
+      <div>
+        <Button>Click me</Button>
+      </div>
     </>
   );
 }
