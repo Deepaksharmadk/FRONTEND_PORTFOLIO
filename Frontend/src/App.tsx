dc14cf87df742ddd884417fe58d1c38b365ca33c
deepaksharmadk <dks847203@gmail.com>
2024-07-28 14:21:51 +0530

    initialize vite and config tailwind css

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl bg-yellow-500 text-red-700 font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
