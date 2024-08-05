26c1894243f8a85fc5d16bef252cda3d46247130
deepaksharmadk <dks847203@gmail.com>
2024-08-06 04:13:27 +0530

    typescript error solved

--- Frontend/src/components/Avatar/Avatar.tsx
+++ Frontend/src/components/Avatar/Avatar.tsx
@@ -1,14 +1,6 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useGetProfileQuery } from "@/redux/slice/userApi";
 
-export interface ProfileData {
-  userId: {
-    avatar: {
-      url: string;
-    };
-  };
-}
-
 export function AvatarFile() {
   const { data } = useGetProfileQuery();
   console.log("data is", data);
